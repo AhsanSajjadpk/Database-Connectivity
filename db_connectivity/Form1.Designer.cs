7317b9a984cfe5d62ca4ec68cedb872e4b94b60b
Ahsan Sajjad <princeahsan0608@gmail.com>
2023-10-07 21:57:52 -0700

    Database Connectivity With VStudio ..

---
+++ db_connectivity/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace db_connectivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.txtdept = new System.Windows.Forms.TextBox();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnupdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(147, 55);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 0;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(147, 91);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Location = new System.Drawing.Point(147, 132);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(100, 20);
+            this.txtaddress.TabIndex = 0;
+            // 
+            // txtdept
+            // 
+            this.txtdept.Location = new System.Drawing.Point(147, 170);
+            this.txtdept.Name = "txtdept";
+            this.txtdept.Size = new System.Drawing.Size(100, 20);
+            this.txtdept.TabIndex = 0;
+            // 
+            // btninsert
+            // 
+            this.btninsert.Location = new System.Drawing.Point(147, 222);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(100, 36);
+            this.btninsert.TabIndex = 1;
+            this.btninsert.Text = "Submit";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(296, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 230);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Department";
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(256, 332);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(125, 35);
+            this.btnupdate.TabIndex = 4;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 393);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.txtdept);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.TextBox txtdept;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnupdate;
+    }
+}
+
---
+++ db_connectivity/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace db_connectivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=WS-015NCMSPL-3;Initial Catalog=Testdb;User ID=sa;Password=bimcs");
+            cn.Open(); 
+            GetAllEmployeeRecord();
+
+
+        }
+
+
+        private void GetAllEmployeeRecord()
+        {
+            cmd = new SqlCommand("GetData",cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // cmd = new SqlCommand("InsertData '"+txtname+"','"+txtaddress+"','"+txtdept+"'  ", cn);
+             cmd = new SqlCommand("InsertData ", cn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name",txtname.Text);
+            cmd.Parameters.AddWithValue("@address", txtaddress.Text);
+            cmd.Parameters.AddWithValue("@dept", txtdept.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully run");
+            txtid.Text =" ";
+            txtname.Text = "";
+            txtaddress.Text = "";
+            txtdept.Text = "";
+
+
+
+            //int a = cmd.ExecuteNonQuery();
+            //if (a>0)
+            //{
+            //    MessageBox.Show("Successfully run");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("failed ");
+            //}
+            //cn.Close();
+
+
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtid.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txtname.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtaddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtdept.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("UpdateData ", cn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.Parameters.AddWithValue("@name", txtname.Text);
+            cmd.Parameters.AddWithValue("@address", txtaddress.Text);
+            cmd.Parameters.AddWithValue("@dept", txtdept.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully run");
+            txtid.Text = " ";
+            txtname.Text = "";
+            txtaddress.Text = "";
+            txtdept.Text = "";
+            GetAllEmployeeRecord();
+        }
+    }
+}
